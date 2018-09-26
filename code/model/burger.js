a37bbc0e8cc0b8f72fed2f639af9c0761bf6cd19
JLasc <lascaze.j@gmail.com>
2018-09-25 22:20:31 -0400

    removed update due to errors

---
+++ code/model/burger.js
@@ -0,0 +1,21 @@
+const orm = require('../config/orm');
+
+const burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res)
+        })
+    },
+    insertOne: function (col, val, cb) {
+        orm.insertOne("burgers", col, val, function (res) {
+            cb(res)
+        })
+    },
+
+
+
+
+
+};
+
+module.exports = burger;
