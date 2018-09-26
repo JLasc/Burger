03994e6713edce2688f21e4c1616a4ada9a8be6c
JLasc <lascaze.j@gmail.com>
2018-09-25 22:19:54 -0400

    updated orm, removed update call due to bugs

--- code/config/orm.js
+++ code/config/orm.js
@@ -1,21 +1,53 @@
-require("./connection");
+const connection = require("./connection");
+
+// Helper function for generating MySQL syntax
+function printQuestionMarks(num) {
+    var arr = [];
+
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// Helper function for generating My SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    for (var key in ob) {
+        arr.push(key + "=" + ob[key]);
+    }
+
+    return arr.toString();
+}
 
 const orm = {
-    all: function (table, cb) {
+    selectAll: function (table, cb) {
         const queryString = 'SELECT * FROM ??';
-        connection.query(queryString, [table], function (req, data) {
+        connection.query(queryString, [table], function (err, data) {
+            if (err) throw err
             cb(data)
         })
     },
-    create: function () {
+    insertOne: function (table, col, val, cb) {
+        var queryString = "INSERT INTO " + table;
+        queryString += " (";
+        queryString += col.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(val.length);
+        queryString += ") ";
+
+        connection.query(queryString, val, function (err, data) {
+            if (err) throw err
+            cb(data)
+        });
 
     },
-    update: function () {
+    // An example of objColVals would be {name: panther, sleepy: true}
 
-    },
-    delete: function () {
 
-    }
 };
 
 module.exports = orm
